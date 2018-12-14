6be77f3e90f56dd945033b7bc72762f361501c83
Christopher Cahill <cthomascahill@gmail.com>
2018-12-14 13:24:49 -0800

    display most recently created items

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
 
   def index
     @product = Product.new()
-    @products = Product.all()
+    @products = Product.most_recent
   end
 
   def create
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ActiveRecord::Base
   has_many :reviews
   validates :name, :cost, :country, :presence => true
+
+  scope :most_recent, -> {(
+    order(created_at: :desc)
+    .limit(3)
+    )}
+
 end
