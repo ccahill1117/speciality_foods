202d16a6484ebd622fbbfab51bf5368f7f24b6cc
Christopher Cahill <cthomascahill@gmail.com>
2018-12-14 13:53:03 -0800

    update cost from integer to float

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_14_192703) do
+ActiveRecord::Schema.define(version: 2018_12_14_215050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "cost"
+    t.float "cost"
     t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
